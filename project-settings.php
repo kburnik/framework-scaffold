bd010a00638fb592539fd35258c6d3d813b83160
Kristijan Burnik <kristijanburnik@gmail.com>
2015-12-24 16:26:02 +0100

    Fix up project and project settings files.

--- project-settings.php
+++ project-settings.php
@@ -1,66 +1,58 @@
 <?php
-
 # FRAMEWORK PROJECT.
-assert_options(ASSERT_ACTIVE, true);
-assert_options(ASSERT_WARNING, false);
-assert_options(ASSERT_CALLBACK, function($script, $line, $message ){
-  echo "Assert failed: $script:$line\n$message\n";
-  file_put_contents("php://stdout", "Assert failed: $script:$line\n$message\n");
-  exit(1);
-});
 
-# DIR
+# DIR.
 define('PROJECT_DIR', dirname(__FILE__));
 define('PATH_TO_FRAMEWORK', constant('PROJECT_DIR') . '/third_party/framework');
 
-# REGIONAL
+# REGIONAL.
 define('PROJECT_LANGUAGE', 'hr');
-define('PROJECT_TIMEZONE', "Europe/Zagreb");
+define('PROJECT_TIMEZONE', 'Europe/Zagreb');
 
-# NAME
+# NAME.
 define('PROJECT_NAME', 'empty-framework-project');
-define('PROJECT_TITLE', "Empty framework project");
-define('PROJECT_DESCRIPTION',
-       "@@PROJECT_DESCRIPTION@@");
+define('PROJECT_TITLE', 'Empty framework project');
+define('PROJECT_DESCRIPTION', 'An empty project for scaffolding.');
+define('PROJECT_DEVELOPMENT_DOMAIN', 'dev.myapp.com');
 
-# AUTHOR
-define('PROJECT_AUTHOR', "Kristijan Burnik");
-define('PROJECT_AUTHOR_MAIL', "kristijanburnik@gmail.com");
+# AUTHOR.
+define('PROJECT_AUTHOR', 'John Doe');
+define('PROJECT_AUTHOR_MAIL', 'johndoe@example.com');
 
-# MYSQL
-define('PROJECT_MYSQL_USERNAME', "");
-define('PROJECT_MYSQL_PASSWORD', "");
-define('PROJECT_MYSQL_TEST_DATABASE', "");
+# DB.
+define('PROJECT_MYSQL_USERNAME', '');
+define('PROJECT_MYSQL_PASSWORD', '');
+define('PROJECT_MYSQL_TEST_DATABASE', '');
 
-# Environment
-if (preg_match("/kristijan/", PROJECT_DIR) || PHP_OS == 'WINNT') {
-  define('ENV', "dev");
+# ENV.
+if (PHP_OS == 'WINNT') {
+  define('ENV', 'dev');
   define('APP_DOMAIN', 'dev.myapp.com');
   define('APP_DEFAULT_PROTOCOL', 'http');
-  define('PROJECT_MYSQL_DATABASE', "");
-} else if (preg_match("/test(.*)/", PROJECT_DIR)) {
-  define('ENV', "canary");
+  define('PROJECT_MYSQL_DATABASE', '');
+} else if (preg_match('/test(.*)/', PROJECT_DIR)) {
+  define('ENV', 'canary');
   define('PRODUCTION_MODE', true);
   define('APP_DOMAIN', 'test.myapp.com');
   define('APP_DEFAULT_PROTOCOL', 'https');
-  define('PROJECT_MYSQL_DATABASE', "");
+  define('PROJECT_MYSQL_DATABASE', '');
 } else {
-  define('ENV', "prod");
+  define('ENV', 'prod');
   define('PRODUCTION_MODE', true);
   define('APP_DOMAIN', 'www.myapp.com');
   define('APP_DEFAULT_PROTOCOL', 'https');
-  define('PROJECT_MYSQL_DATABASE', "");
+  define('PROJECT_MYSQL_DATABASE', '');
 }
 
-# VIEW
+# VIEW.
 define('PROJECT_VIEW_DIR', constant('PROJECT_DIR') . '/view');
-define("URL_ROOT", "");
-define("REQUEST_URI", $_SERVER["REQUEST_URI"]);
-define("HTTP_HOST", $_SERVER["HTTP_HOST"]);
-define("SERVER_PROTOCOL", $_SERVER["SERVER_PROTOCOL"]);
-define("REQUEST_METHOD", $_SERVER["REQUEST_METHOD"]);
-define("URL_QUERY", ($_SERVER['QUERY_STRING']) ?
-                    '?' . $_SERVER['QUERY_STRING'] : "");
+define('URL_ROOT', '');
+define('REQUEST_URI', $_SERVER['REQUEST_URI']);
+define('HTTP_HOST', $_SERVER['HTTP_HOST']);
+define('SERVER_PROTOCOL', $_SERVER['SERVER_PROTOCOL']);
+define('REQUEST_METHOD', $_SERVER['REQUEST_METHOD']);
+define('URL_QUERY', ($_SERVER['QUERY_STRING']) ?
+                    '?' . $_SERVER['QUERY_STRING'] : '');
 
 define('PDF_OUTPUT_DIR', constant('PROJECT_DIR') . '/gen/pdf');
 define('PROJECT_DATA_DIR', constant('PROJECT_DIR') . '/data');
--- project.php
+++ project.php
@@ -1,5 +1,13 @@
 <?php
 
+assert_options(ASSERT_ACTIVE, true);
+assert_options(ASSERT_WARNING, false);
+assert_options(ASSERT_CALLBACK, function($script, $line, $message ){
+  echo "Assert failed: $script:$line\n$message\n";
+  file_put_contents("php://stdout", "Assert failed: $script:$line\n$message\n");
+  exit(1);
+});
+
 include_once(dirname(__FILE__) . '/project-settings.php');
 include_once(constant('PATH_TO_FRAMEWORK') . '/base/Base.php');
 
